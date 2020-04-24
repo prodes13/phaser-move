106dae37b12acf5ebf8fba047b89c519ad9bb77f
prodes13 <ciobotaru.florin@gmail.com>
2020-04-24 11:22:52 +0300

    game tutorial finished

--- server.js
+++ server.js
@@ -23,6 +23,14 @@ const io = socketio(server);
 
 // checking waiting players
 let players = {};
+let star = {
+    x: Math.floor(Math.random() * 700) + 50,
+    y: Math.floor(Math.random() * 500) + 50
+  };
+let scores = {
+    blue: 0,
+    red: 0
+  };
 
 io.on('connection', (socket) => {
     console.log('A user connected!');
@@ -37,6 +45,10 @@ io.on('connection', (socket) => {
     };
     // send the players object to the new player
     socket.emit('currentPlayers', players);
+    // send the star object to the new player
+    socket.emit('starLocation', star);
+    // send the current scores
+    socket.emit('scoreUpdate', scores);
     // update all other players of the new player
     socket.broadcast.emit('newPlayer', players[socket.id]);
 
@@ -49,13 +61,25 @@ io.on('connection', (socket) => {
         socket.broadcast.emit('playerMoved', players[socket.id]);
     });
 
+    socket.on('starCollected', function () {
+        if (players[socket.id].team === 'red') {
+          scores.red += 10;
+        } else {
+          scores.blue += 10;
+        }
+        star.x = Math.floor(Math.random() * 700) + 50;
+        star.y = Math.floor(Math.random() * 500) + 50;
+        io.emit('starLocation', star);
+        io.emit('scoreUpdate', scores);
+      });
+
     socket.on('disconnect', () => {
         console.log('A user disconnected!');
         // remove this player from our players object
         delete players[socket.id];
         // emit a message to all players to remove this player
         io.emit('disconnect', socket.id);
-    })
+    });
 });
 
 
